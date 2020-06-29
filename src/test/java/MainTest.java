fad039eb1f056b379436c1589e8b6fa3c2bca605
Julia Contreras <juliacontreras483@gmail.com>
2020-06-29 16:54:24 -0500

    making class

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+public class MainTest {
+}
+
+
+//    Create a new class with a main method that prompts the user for input and does the following (using the Stringutils class)
+//
+//        Tells whether or not what the user entered is a number
+//        Flips the case of the string
+//        Reverses the string
+//        Example Console Output
+
+//    Enter Something: Life, The Universe, and Everything
+//        You Entered: "Life, The Universe, and Everything"
+//        "Life, The Universe, and Everything" is not a number
+//        Flipped Case: lIFE, tHE uNIVERSE, AND eVERYTHING
+//        Reversed: gnihtyrevE dna ,esrevinU eht ,efiL
